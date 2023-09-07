cc111df29bcd870a6b24ee200c94489d3842130f
gesarizky <gesarizkynugraha@gmail.com>
2023-09-07 15:53:58 +0700

    routing fixed

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,6 @@
 import dataHistory from "./history.js";
 import dataSetting from "./settings.js";
 import dataFetching from "./fetching.js";
-import mainRms from "../controller/get/mainRms.js";
-import mainInverter from "../controller/get/mainInverter.js";
-import mainMppt from "../controller/get/mainMppt.js";
 
 export default (app) => {
   app.get("/", async (req, res) => {
