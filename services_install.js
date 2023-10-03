f09290f897711d9105e54a1f41f382fe87830e20
gesarizky <gesarizkynugraha@gmail.com>
2023-10-03 08:35:06 +0700

    + node windows w/out daemon

---
+++ services_install.js
@@ -0,0 +1,13 @@
+import { Service } from "node-windows";
+
+const svc = new Service({
+  name: "Talis30 Data Post",
+  description: "This is program node.js for talis30.",
+  script: "C:\\Talis30\\data-collect\\talis30-client-datacollect\\index.js", //path program nodejs change \ to \\
+});
+
+svc.on("install", () => {
+  svc.start();
+});
+
+svc.install();
